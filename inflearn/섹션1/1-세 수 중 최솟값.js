d75392286f2bc8e9db988b0704de4763d6dcd3a7
sanghee01 <sang6299@naver.com>
2022-07-23 23:01:19 +0900

    22/07/23

--- inflearn/섹션1/1-세 수 중 최솟값.js
+++ inflearn/섹션1/1-세 수 중 최솟값.js
@@ -18,6 +18,35 @@ function solution(a, b, c) {
 
 console.log(solution(2, 5, 1));
 
+
+# 다른 풀이 - 굳이 안따져도 되는거에서 조건을 따짐
+function solution(a, b, c) {
+  let answer;
+  if (a > b && a > c) {
+    if (b > c) {
+      answer = c;
+    } else {
+      answer = b;
+    }
+  } else if (b > a && b > c) {
+    if (a > c) {
+      answer = c;
+    } else {
+      answer = a;
+    }
+  } else {
+    if (a > b) {
+      answer = b;
+    } else {
+      answer = a;
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(2, 5, 1));
+
 */
 
 function solution(a, b, c) {
