f309b1578bcd72cdd6c9d9f67e15bd250447f25c
sanghee01 <sang6299@naver.com>
2024-01-24 01:36:21 -0800

    solve: BOJ 10953 B3

---
+++ 코테준비/7-10953.js
@@ -0,0 +1,12 @@
+const input = require("fs")
+  .readFileSync("input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const T = Number(input[0]);
+for (let i = 1; i <= T; i++) {
+  const A = Number(input[i].split(",")[0]);
+  const B = Number(input[i].split(",")[1]);
+  console.log(A + B);
+}
